7695ed2fc586dc2c5907219f070e7708c09ce473
rajesh rohan <rajeshrohan62@gmail.com>
2024-05-14 16:47:53 +0530

    server responds to req dynamically

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ import fs from 'fs';
 
 const index = fs.readFileSync('index.html', 'utf-8');          
 const data = JSON.parse(fs.readFileSync('data.json'));  
-const product = data.products[3]; 
+const products = data.products; 
 
-// above lines are outside server, so runs only one time. 
+// above lines are outside server, so runs only one time.
+
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  console.log(req.url, req.method);
+
+  if(req.url.startsWith('/product')){
+    const id = req.url.split('/')[2];
+    const product = products.find(p => p.id === (+id));
+
+    res.setHeader('content-type', 'text/html');
+      let modifiedIndex = index.replace('**title**', product.title).
+      replace('**price**', product.price).
+      replace('**rating**', product.rating).
+      replace('**url**',product.thumbnail);
+      
+      res.end(modifiedIndex);
+      return;
+  }
+
   switch(req.url){
     case '/': 
       res.setHeader("content-type", "text/html");
@@ -17,15 +33,6 @@ const server = http.createServer((req, res) => {
       res.setHeader('content-type', 'application/json');
       res.end(JSON.stringify(data)); 
       break;
-    case '/product' :
-      res.setHeader('content-type', 'text/html');
-      let modifiedIndex = index.replace('**title**', product.title).
-      replace('**price**', product.price).
-      replace('**rating**', product.rating).
-      replace('**url**',product.thumbnail);
-
-      res.end(modifiedIndex);
-      break;
     default:
       res.writeHead(400);
       res.end();
@@ -33,5 +40,3 @@ const server = http.createServer((req, res) => {
   console.log("server started");
 });
 server.listen(8080);
-
-

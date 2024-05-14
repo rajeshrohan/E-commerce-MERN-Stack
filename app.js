3b532db1cca61a75adc18a0520393b8e0cbca147
rajesh rohan <rajeshrohan62@gmail.com>
2024-05-14 16:16:59 +0530

    server with dynamic response altering json

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import http from "http";
+import http from 'http';
 import fs from 'fs';
 
-const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-const product = data.products[0];
+const index = fs.readFileSync('index.html', 'utf-8');          
+const data = JSON.parse(fs.readFileSync('data.json'));  
+const product = data.products[3]; 
 
-
+// above lines are outside server, so runs only one time. 
 const server = http.createServer((req, res) => {
   console.log(req.url);
   switch(req.url){
@@ -15,14 +15,17 @@ const server = http.createServer((req, res) => {
       break;
     case '/api' : 
       res.setHeader('content-type', 'application/json');
-      res.end(JSON.stringify(data));
+      res.end(JSON.stringify(data)); 
       break;
     case '/product' :
       res.setHeader('content-type', 'text/html');
-      let modifiedIndex = index.replace('**title**', product.titile);
+      let modifiedIndex = index.replace('**title**', product.title).
+      replace('**price**', product.price).
+      replace('**rating**', product.rating).
+      replace('**url**',product.thumbnail);
+
       res.end(modifiedIndex);
       break;
-
     default:
       res.writeHead(400);
       res.end();

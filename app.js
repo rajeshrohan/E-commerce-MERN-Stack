ef4b9950e06795f87ac85b847dd2966834e7c6e6
rajesh rohan <rajeshrohan62@gmail.com>
2024-05-20 11:30:00 +0530

    mongoDB connection and Environment Vairables set

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const server = express(); 
 const productRouter = require('./routes/product');
 const userRouter = require('./routes/user')
 
-// body parser
-server.use(express.json()); 
-server.use(morgan('dev'));
-server.use(express.static('public')); 
+//console.log(process.env.DB_PASSWORD);
 
+// middleware 
+server.use(express.json());  // body parser
+server.use(morgan('dev'));  // logger 
+server.use(express.static('public'));  // hoisting
+
+// Route handlers 
 server.use('/products', productRouter.router);
 server.use('/users', userRouter.router);
 
-server.listen(8080, ()=>{   
+server.listen(process.env.PORT, ()=>{   
   console.log('server running on port 8080');
 });
-
-// while using any route, we can give a prefix url or base url to that 
